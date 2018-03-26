20a2ee037502cf67d1d45e922263ecb0dd8b29b7
discoder <discoder.hello@gmail.com>
2018-03-26 16:00:23 +0300

    Добавляет некоторый дефолтный КСС для красоты.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,24 @@
-console.log('Hello from index.js')
+(function() {
+
+    var obj = {};
+    var ObjectWithoutProto = Object.create(null);
+
+    function func () {
+        console.log("Была вызвана функция 'func'");
+        return true;
+    }
+
+    var user = {
+        role: "user"
+    };
+    var userIvan = {
+        login: "userivan"
+    }
+    userIvan.__proto__ = user;
+
+
+    var arr = [1,2,3];
+
+
+    console.log( Array.prototype )
+})()
