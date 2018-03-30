9b4f5c95a5a27eab7571cbe7b3c72edfd1970034
discoder <discoder.hello@gmail.com>
2018-03-30 19:37:04 +0300

    Save commit.

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,6 @@
 (function() {
-
-    var obj = {};
-    var ObjectWithoutProto = Object.create(null);
-
-    function func () {
-        console.log("Была вызвана функция 'func'");
-        return true;
-    }
-
-    var user = {
-        role: "user"
-    };
-    var userIvan = {
-        login: "userivan"
-    }
-    userIvan.__proto__ = user;
-
-
-    var arr = [1,2,3];
-
-
-    console.log( Array.prototype )
+    var button = document.getElementById('click-me-button')
+    button.addEventListener("click", (event) => {
+        console.log(event.target)
+    })
 })()
