fbce0837757a1e772d727641dde4913d60aa0844
discoder <discoder.hello@gmail.com>
2018-04-06 18:16:20 +0300

    Save commit.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,20 @@
 (function() {
     var button = document.getElementById('click-me-button')
+
+    var boo = function boo() {
+        console.log('boo inside foo')
+    }
+
+    var foo = function foo(payload) {
+        payload()
+    }
+
+    var obj = {
+        testField: 2317
+    }
+
     button.addEventListener("click", (event) => {
-        console.log(event.target)
+        foo(boo)
     })
+
 })()
